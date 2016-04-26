a032b523573dd0c82c54875463dec7d88e20f171
Severen Redwood <severen@shrike.me>
2016-04-26 17:55:28 +1200

    Move Fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,10 +3,10 @@ set -x VISUAL nvim
 set -x EDITOR $VISUAL
 set -x SUDO_EDITOR $VISUAL
 
-alias edit 'eval $VISUAL'
+alias edit "eval $VISUAL"
 
 ## XDG Base Directory Specification workarounds.
-if which 'ncmpcpp' > '/dev/null'
+if which ncmpcpp > /dev/null
   alias ncmpcpp "ncmpcpp -c $XDG_CONFIG_HOME/ncmpcpp/config"
 end
 
@@ -14,8 +14,8 @@ end
 alias gpl 'git log --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s \
 %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
 
-if which 'git' > '/dev/null'; and which 'hub' > '/dev/null'
-  alias 'git' 'hub'
+if which git > /dev/null; and which hub > /dev/null
+  alias git hub
 end
 
 ## Fisherman Plugins
@@ -24,7 +24,7 @@ set -U Z_DATA $XDG_DATA_HOME/z
 
 ## Misc.
 set -x GOPATH $HOME/Documents/Go
-set fish_greeting # Goodbye welcome message!
+set fish_greeting
 
 # Have Tmux re-attach to the Tmux the main session.
 function tmux
