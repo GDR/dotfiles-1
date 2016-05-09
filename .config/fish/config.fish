be2f88afb99098d764e708393310c3ba2ea82da7
Severen Redwood <severen@shrike.me>
2016-05-09 22:37:07 +1200

    Export env. vars. from ~/.profile

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,32 +4,6 @@ if status --login
 end
 set -x GOPATH $HOME/Documents/Go
 
-# XDG BaseDir
-set -x XDG_CONFIG_HOME $HOME/.config
-set -x XDG_DATA_HOME $HOME/.local/share
-set -x XDG_CACHE_HOME $HOME/.cache
-
-set -x GNUPGHOME $XDG_CONFIG_HOME/gnupg
-set -x CCACHE_DIR $XDG_CACHE_HOME/ccache
-set -x PYENV_ROOT $XDG_DATA_HOME/pyenv
-set -x RBENV_ROOT $XDG_DATA_HOME/rbenv
-set -x CARGO_HOME $XDG_DATA_HOME/cargo
-set -x RUSTUP_HOME $XDG_DATA_HOME/rustup
-set -x HTTPIE_CONFIG_DIR $XDG_CONFIG_HOME/httpie
-set -x IPYTHONDIR $XDG_CONFIG_HOME/ipython
-set -x JUPYTER_CONFIG_DIR $XDG_CONFIG_HOME/jupyter
-if which tmux > /dev/null and not test -d $XDG_RUNTIME_DIR/tmux
-  mkdir $XDG_RUNTIME_DIR/tmux
-end
-set -x TMUX_TMPDIR $XDG_RUNTIME_DIR/tmux
-set -x STACK_ROOT $XDG_CONFIG_HOME/stack
-set -x WEECHAT_HOME $XDG_CONFIG_HOME/weechat
-if not test -d $XDG_CACHE_HOME/nv
-  mkdir $XDG_CACHE_HOME/nv
-end
-set -x __GL_SHADER_DISK_CACHE_PATH $XDG_CACHE_HOME/nv
-set -x CUDA_CACHE_PATH $XDG_CACHE_HOME/nv
-
 # Plugins #
 set -U Z_DATA $XDG_DATA_HOME/z
 
